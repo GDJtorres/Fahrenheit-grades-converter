6e57d3cf66426190cca8a6f9a677ded279a248aa
Gerardo Torres <torres.gerardo.torres@gmail.com>
2024-04-02 05:58:39 -0500

    proyect

---
+++ JavaScript/script.js
@@ -0,0 +1,16 @@
+const inputData = document.querySelector("input");
+const button = document.querySelector("button");
+function convertFahrenheit ()
+    { return inputData.value * 9/5 + 32};
+
+function addSpace(){
+    const targetDiv = document.querySelector("#result-container");
+    const fahrenheit = convertFahrenheit();
+    const HTMLString = `
+    <p id="${fahrenheit}">${inputData.value} fahrenheit grades, converted to Celsius are ${fahrenheit}</p>
+    `
+    targetDiv.innerHTML += HTMLString; 
+}
+
+button.addEventListener("click", addSpace);
+
