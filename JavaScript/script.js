1c06790ba6644a5f6031cd6032126568927609a0
Gerardo Torres <torres.gerardo.torres@gmail.com>
2024-04-02 21:51:43 -0500

    P

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -3,15 +3,16 @@ const button = document.querySelector("button");
 function convertFahrenheit ()
     { return inputData.value * 9/5 + 32};
 
-    
+    //this is the function which add the new space with the result
 function addSpace(){
     const targetDiv = document.querySelector("#result-container");
     const fahrenheit = convertFahrenheit();
     const HTMLString = `
-    <p id="${fahrenheit}">${inputData.value} fahrenheit grades, converted to Celsius are ${fahrenheit}</p>
+    <p id="${fahrenheit}">${inputData.value} Celsius grades, converted to fahrenheit are ${fahrenheit}</p>
     `
     targetDiv.innerHTML += HTMLString; 
 }
 
+//with this we click on the button to call the function above
 button.addEventListener("click", addSpace);
 
